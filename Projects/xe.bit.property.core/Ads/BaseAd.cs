3dc079e8ce21b5c74563d0fc5d68b0db98d3f16e
n.bitounis <n.bitounis@pamediakopes.gr>
2019-03-28 20:56:49 +0200

    Added base ad class.

---
+++ Projects/xe.bit.property.core/Ads/BaseAd.cs
@@ -0,0 +1,25 @@
+﻿using xe.bit.property.core.Iso;
+using xe.bit.property.core.Lookups;
+
+namespace xe.bit.property.core.Ads
+{
+	public class BaseAd
+	{
+		public virtual ItemType Type { get; set; }
+		public virtual string RefId { get; set; }
+		public virtual string OwnerId { get; set; }
+		public virtual string MajorPhone { get; set; }
+		public virtual string DepartmentOnCategory { get; protected set; } = "Real Estate";
+		public virtual string[] OtherPhones { get; set; } = { };
+		public virtual string InternetText { get; set; }
+		public virtual string AddOnText { get; set; }
+		public virtual string BodyText { get; set; }
+		public virtual decimal Price { get; set; }
+		public virtual CurrencyCode Currency { get; set; }
+		public virtual TransactionFrequency Frequency { get; set; }
+		public virtual TransactionType TransactionType { get; set; }
+		public bool? IsOffer { get; set; }
+		public bool? IsPromo { get; set; }
+		public bool? IsNegotiable { get; set; }
+	}
+}
