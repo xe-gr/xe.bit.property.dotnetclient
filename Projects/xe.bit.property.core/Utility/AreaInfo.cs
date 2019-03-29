7b68287fcd6ab44456f8669b277b264109e363a7
n.bitounis <n.bitounis@pamediakopes.gr>
2019-03-29 07:38:03 +0200

    Added area information and related utility class.

---
+++ Projects/xe.bit.property.core/Utility/AreaInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using xe.bit.property.core.Properties;
+
+namespace xe.bit.property.core.Utility
+{
+	public class AreaInfo
+	{
+		private static Dictionary<string, AreaProperty> AreaMap { get; }
+		private static List<string> Exceptions { get; }
+
+		static AreaInfo()
+		{
+			AreaMap = new Dictionary<string, AreaProperty>();
+			Exceptions = new List<string>();
+
+			var fileInfo = Resources.AreaInfo;
+			var lines = fileInfo.Split("\r\n");
+			foreach (var line in lines)
+			{
+				if (line.StartsWith("Map_Id") || string.IsNullOrEmpty(line)) continue;
+				var fields = line.Split(new[] {'\t'});
+				var property = new AreaProperty {MapId = fields[0], MapLabel = string.IsNullOrEmpty(fields[3]) ? fields[1] : $"{fields[1]}, {fields[3]}"};
+				AreaMap.Add(property.MapId, property);
+			}
+
+			fileInfo = Resources.AreaExceptions;
+			lines = fileInfo.Split("\r\n");
+			foreach (var line in lines)
+			{
+				if (string.IsNullOrEmpty(line)) continue;
+				Exceptions.Add(line);
+			}
+
+			Exceptions.Sort();
+		}
+
+		public static int NumberOfExceptions => Exceptions.Count;
+
+		public static bool IsAreaIdException(string areaId) => Exceptions.BinarySearch(areaId) >= 0;
+
+		public static bool IsAreaIdValid(string areaId) => AreaMap.ContainsKey(areaId);
+
+		public static string GetMapLabelOfAreaId(string areaId)
+		{
+			if (!AreaMap.TryGetValue(areaId, out var property))
+			{
+				throw new InvalidOperationException($"AreaId {areaId} is not valid");
+			}
+
+			return property.MapLabel;
+		}
+
+		internal class AreaProperty
+		{
+			public string MapId { get; set; }
+			public string MapLabel { get; set; }
+		}
+	}
+}
---
+++ Tests/xe.bit.property.core.tests/Utility/AreaInfoTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using xe.bit.property.core.Utility;
+using Xunit;
+
+namespace xe.bit.property.core.tests.Utility
+{
+	public class AreaInfoTests
+	{
+		[Fact]
+		public void VerifyNumberOfAreaExceptions()
+		{
+			Assert.Equal(33, AreaInfo.NumberOfExceptions);
+		}
+
+		[Fact]
+		public void VerifyAreaExceptions()
+		{
+			var exceptions = new List<string>
+			{
+				"59-15", "59-23", "59-26", "59-3", "59-44", "47", "65", "66", "67", "68", "58", "59", "60", "61", "62",
+				"63", "64", "13", "14", "36", "48", "50", "56", "88", "89", "91", "94", "95", "96", "97", "98", "99",
+				"100000"
+			};
+
+			foreach (var exception in exceptions)
+			{
+				Assert.True(AreaInfo.IsAreaIdException(exception));
+			}
+		}
+
+		[Fact]
+		public void SampleAreaInfoParsed()
+		{
+			Assert.True(AreaInfo.IsAreaIdValid("1133"));
+			Assert.True(AreaInfo.IsAreaIdValid("58"));
+		}
+
+		[Fact]
+		public void VerifyAreaInfoDetectsInvalidArea()
+		{
+			Assert.False(AreaInfo.IsAreaIdValid("Map_Id"));
+		}
+
+		[Fact]
+		public void SampleAreaInfoLabel()
+		{
+			Assert.Equal("Άγιος Σώστης, Νέος Κόσμος", AreaInfo.GetMapLabelOfAreaId("59-28-105"));
+			Assert.Equal("Θράκη", AreaInfo.GetMapLabelOfAreaId("96"));
+		}
+	}
+}
