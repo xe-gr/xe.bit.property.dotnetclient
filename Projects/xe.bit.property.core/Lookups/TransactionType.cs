963e88011f59dd3d2d109f745267a517c6cfc146
n.bitounis <n.bitounis@pamediakopes.gr>
2019-04-07 23:48:55 +0300

    Added description attribute for serialization purposes.

--- Projects/xe.bit.property.core/Lookups/TransactionType.cs
+++ Projects/xe.bit.property.core/Lookups/TransactionType.cs
@@ -1,17 +1,30 @@
-﻿namespace xe.bit.property.core.Lookups
+﻿using System.ComponentModel;
+
+namespace xe.bit.property.core.Lookups
 {
 	public enum TransactionType
 	{
+		[Description("SELL.NORMAL")]
 		SELL_NORMAL,
+		[Description("SELL.EXCHANGE")]
 		SELL_EXCHANGE,
+		[Description("SELL.AUCTION")]
 		SELL_AUCTION,
+		[Description("SELL.GIFT")]
 		SELL_GIFT,
+		[Description("SELL.ANTIPAROXI")]
 		SELL_ANTIPAROXI,
+		[Description("BUY.NORMAL")]
 		BUY_NORMAL,
+		[Description("BUY.EXCHANGE")]
 		BUY_EXCHANGE,
+		[Description("BUY.GIFT")]
 		BUY_GIFT,
+		[Description("BUY.ANTIPAROXI")]
 		BUY_ANTIPAROXI,
+		[Description("RENT.NORMAL")]
 		RENT_NORMAL,
+		[Description("LET.NORMAL")]
 		LET_NORMAL
 	}
 }
--- Projects/xe.bit.property.core/Request/PackagePolicy.cs
+++ Projects/xe.bit.property.core/Request/PackagePolicy.cs
@@ -1,7 +1,10 @@
-﻿namespace xe.bit.property.core.Request
+﻿using System.ComponentModel;
+
+namespace xe.bit.property.core.Request
 {
 	public enum PackagePolicy
 	{
+		[Description("RENEW ALL STOCK")]
 		RENEW_ALL_STOCK,
 		INCREMENTAL
 	}
